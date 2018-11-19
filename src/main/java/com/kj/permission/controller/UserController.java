56ac89485088b00c9405427a7e3866d5e02c1ce1
fanyalong <fanyalong@wondertek.com.cn>
2018-11-19 17:11:46 +0800

    主干第二次提交代码

--- src/main/java/com/kj/permission/controller/UserController.java
+++ src/main/java/com/kj/permission/controller/UserController.java
@@ -232,7 +232,7 @@ public class UserController {
 	public String goUserIndexJsp2(){
 		//查询所有user
 		//List<User> users = userService.getAllUser();
-		
+		System.out.println("我是主干第二次提交代码");
 		return "user/index";
 	}
 }
